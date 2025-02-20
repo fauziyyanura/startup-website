cc7b2081f5bd45396a5c6aa80638a55efb9bd5db
Your Name <fauziyyanuraahmad@gmail.com>
2025-02-20 01:03:57 +0100

    Add us.js

---
+++ src/us.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const US = () => {
+    return (
+        <div className='contact-us'>
+            <h1>Contact us</h1>
+            <p> Lorem ipsum , dolor sit amet consectetur adipisicingg elit</p>
+
+        </div>
+    );
+};
+
+export default US;
