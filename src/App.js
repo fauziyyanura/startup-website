ecb83c42639d903db06e407c8a8a8c13fd62404b
Your Name <fauziyyanuraahmad@gmail.com>
2025-02-20 01:05:20 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './home';
 import Portfolio from './portfolio' 
+import Service from './service'; 
+import Contact from './contact'; 
 
 
 const App = () => {
@@ -11,6 +13,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} /> 
         <Route path="/portfolio" element={<Portfolio />} /> 
+        <Route path="/service" element={<Service />} /> 
+        <Route path="/contact" element={< Contact/>} />  
+
         </Routes>
     </Router>
   );
