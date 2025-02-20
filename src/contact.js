7bebfac3027c12f68e9a18e13d8251cbd558f4b7
Your Name <fauziyyanuraahmad@gmail.com>
2025-02-20 01:04:42 +0100

    Add contact.js

---
+++ src/contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Nav from './nav'
+import Us from './us';
+import Footer from './footer';
+
+const CONTACT = () => {
+    return (
+        <div>
+            <Nav />
+            <main>
+                <Us />
+            </main>
+            <Footer />
+        </div>
+    );
+};
+
+export default CONTACT;
